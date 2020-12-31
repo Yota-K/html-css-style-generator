76cfa369249f541b0600e4ef9341142562207c18
紅梅陽太 <powdersugar828828@gmail.com>
2021-01-01 03:20:05 +0900

    テンプレートの作成。

---
+++ src/components/Pages/CreateButtonPage/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AppTemplate } from '../../Templates/AppTemplate/index';
+
+export const CreateButtonPage = () => {
+  return <AppTemplate title="Style Generator｜ボタンの作成" />;
+};
