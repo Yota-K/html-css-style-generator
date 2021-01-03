77972a389af6aa61f8793164aa821e9626ae5b9d
紅梅陽太 <powdersugar828828@gmail.com>
2021-01-03 10:42:14 +0900

    anyだった部分に型を指定。

--- src/components/Atoms/ButtonGenerator/Code/CodeGenerate.ts
+++ src/components/Atoms/ButtonGenerator/Code/CodeGenerate.ts
@@ -20,14 +20,17 @@ ${styles}}\n`;
   }
 };
 
+type ObjType = State['defaultStyles'] | State['hoverStyles'] | State['activeStyles'];
+
 // 文字列の整形
-const replaceStr = (state: any) => {
-  const ary = Object.values(state);
+const replaceStr = (obj: ObjType) => {
+  // オブジェクトになっているので配列に変換
+  const ary = Object.values(obj);
 
+  // 整形後の文字列を生成するため新しい配列を生成
   let str = '';
-  str += ary.map((e) => {
-    return `  ${e}\n`;
-  });
+  str += ary.map((e) => `  ${e}\n`);
 
+  // カンマが邪魔なので削除する
   return str.replace(/,/g, '');
 };
