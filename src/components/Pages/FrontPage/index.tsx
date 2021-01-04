b7de7a5da1c5d13805dee8f6987f5793f1623dd7
Yota-K <51050458+Yota-K@users.noreply.github.com>
2021-01-04 20:31:29 +0900

    表示とスタイル周りの修正。

--- src/components/Pages/FrontPage/index.tsx
+++ src/components/Pages/FrontPage/index.tsx
@@ -9,16 +9,16 @@ const cardData: CardDataProps[] = [
     description: 'HTML・CSSを書かずにボタンの作成ができます。',
     path: 'create-button',
   },
-  {
-    heading: '見出しの作成',
-    description: 'HTML・CSSを書かずに見出しの作成ができます。',
-    path: 'create-heading',
-  },
-  {
-    heading: 'オリジナルのスタイルの作成',
-    description: 'HTML・CSSをを使用して、オリジナルのスタイルの作成ができます。',
-    path: 'create-original-style',
-  },
+  /* {                                                                                                          */
+  /*   heading: '見出しの作成',                                                                           */
+  /*   description: 'HTML・CSSを書かずに見出しの作成ができます。',                            */
+  /*   path: 'create-heading',                                                                                  */
+  /* },                                                                                                         */
+  /* {                                                                                                          */
+  /*   heading: 'オリジナルのスタイルの作成',                                                      */
+  /*   description: 'HTML・CSSをを使用して、オリジナルのスタイルの作成ができます。', */
+  /*   path: 'create-original-style',                                                                           */
+  /* },                                                                                                         */
 ];
 
 export const FrontPage = () => {
